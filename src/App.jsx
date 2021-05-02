bd7d41c180f7e0ac00d85b502334776bb48e36e8
hakkeiy <38604374+hakkeiy@users.noreply.github.com>
2021-05-02 23:30:16 +0900

    todo数制限

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,14 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できる数は5個まで！消化してください。
+        </p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
